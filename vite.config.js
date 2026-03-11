ed931e41b298413032ed4f516d28d786a196f217
lucaa06 <lucozzopazzo@gmail.com>
2026-03-11 19:18:53 +0100

    Fix: configurazione automatica per deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-const { defineConfig } = require('vite')
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-module.exports = defineConfig({
-    server: {
-        host: '0.0.0.0',
-        port: 5173,
-    }
+export default defineConfig({
+  plugins: [react()],
+  base: '/WebAgencyGenova/',
 })
